6778100f745fc594fc293deafd8fd5be8550cb7a
Andrea Vos <andrea@avris.it>
2019-12-19 19:01:06 +0000

    allow controlling with keyboard

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -6,15 +6,15 @@
         </template>
 
         <template v-slot:col1>
-            <ListSelect v-model="attraction" :options="attractionTypes"/>
+            <ListSelect v-model="attraction" :options="attractionTypes" ref="col0"/>
         </template>
 
         <template v-slot:col2>
-            <ListSelect v-model="relationship" :options="relationshipTypes"/>
+            <ListSelect v-model="relationship" :options="relationshipTypes" ref="col1"/>
         </template>
 
         <template v-slot:col3>
-            <ListSelect v-model="orientation" :options="orientationTypes"/>
+            <ListSelect v-model="orientation" :options="orientationTypes" ref="col2"/>
         </template>
     </Layout>
 </template>
@@ -34,14 +34,44 @@
                 relationship: null,
                 orientation: null,
 
-                attractionTypes, relationshipTypes, orientationTypes
+                attractionTypes, relationshipTypes, orientationTypes,
+
+                activeColumn: 0,
             }
+        },
+        mounted() {
+            window.addEventListener('keydown', this.handleKey, false);
+        },
+        destroyed() {
+            window.removeEventListener('keydown', this.handleKey, false);
         },
         methods: {
             clear() {
                 this.attraction = null;
                 this.relationship = null;
                 this.orientation = null;
+            },
+            handleKey(e) {
+                switch (e.key) {
+                    case 'ArrowLeft':
+                        this.activeColumn = Math.max(0, this.activeColumn - 1);
+                        this.$refs['col' + this.activeColumn].focus();
+                        e.preventDefault();
+                        break;
+                    case 'ArrowRight':
+                        this.activeColumn = Math.min(2, this.activeColumn + 1);
+                        this.$refs['col' + this.activeColumn].focus();
+                        e.preventDefault();
+                        break;
+                    case 'ArrowUp':
+                        this.$refs['col' + this.activeColumn].move(-1);
+                        e.preventDefault();
+                        break;
+                    case 'ArrowDown':
+                        this.$refs['col' + this.activeColumn].move(1);
+                        e.preventDefault();
+                        break;
+                }
             },
         },
         computed: {
--- src/components/ListSelect.vue
+++ src/components/ListSelect.vue
@@ -1,10 +1,11 @@
 <template>
     <div>
         <h2 class="mt-3 mt-sm-0 mb-3">{{ $t(`${options.label}.label`) }}</h2>
-        <div class="list-group">
+        <div class="list-group" ref="list" tabindex="0">
             <a href :class="['list-group-item list-group-item-action', {active: v === o.code, disabled}]"
                v-for="o in options.types"
                @click.prevent="update(o.code)"
+               ref="item"
             >
                 <h3 class="h5 font-weight-bold">
                     {{ o.label || o.code }} – {{ $t(`${options.label}.${o.code}.name`) }}
@@ -33,14 +34,34 @@
         },
         methods: {
             update(v) {
+                return this.emit(v === this.value ? null : v);
+            },
+            emit(v) {
                 if (!this.disabled) {
-                    this.$emit('input', v === this.value ? null : v);
+                    this.$emit('input', v);
                 }
+            },
+            move(diff) {
+                const next = this.findIndex(this.v) + diff;
+                return next < 0 || next >= this.options.types.length
+                    ? this.emit(null)
+                    : this.emit(this.options.types[next].code);
+            },
+            findIndex(v) {
+                return this.options.types.findIndex(o => o.code === v);
+            },
+            focus() {
+                this.$refs.list.focus();
             },
         },
         watch: {
             value(v) {
                 this.v = v;
+                if (v) {
+                    this.$refs.item[this.findIndex(v)].focus();
+                } else {
+                    this.$refs.list.focus();
+                }
             }
         },
     }
