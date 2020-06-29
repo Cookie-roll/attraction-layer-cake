13f10769ca84e3dc80bbc865a489e2cfd9839058
Ana Fernandez Serrano <1539825-Anathea@users.noreply.gitlab.com>
2020-06-29 13:39:38 +0000

    Add French translation

---
+++ src/translations/fr.js
@@ -0,0 +1,85 @@
+module.exports = {
+    'title': 'Le gâteau de la sexualité',
+    'description': 'Notre sexualité est bien plus vaste que simplement hétéro/bi/homo. Ici vous pouvez la décrire sur trois axes : type d\'attirance, type de relation et orientation sexuelle.',
+    'keywords': 'attirance, relation, orientation, sexualité, orientation sexuelle, échelle, échelle de kinsey, asexualité, asexuel⋅le, monogamie, polyamour',
+
+    'footer.author': 'Fait avec le ❤ par',
+    'footer.base': 'basé sur',
+    'footer.diagram': 'un diagramme de Luna Rudd',
+    'footer.donate': 'Offrez-moi une bière',
+    'footer.source': 'Code source',
+
+    'attraction.label': 'Type d\'attirance',
+    'attraction.b.name': 'Asexualité aromantique',
+    'attraction.b.description': 'Aucune attirance sexuelle ni romantique, uniquement platonique ou esthétique.',
+    'attraction.g.name': 'Asexualité romantique',
+    'attraction.g.description': 'Intérêt pour l\'affection, certains contacts et des liens émotionnels puissants ' +
+                                'mais pas d\'intérêt pour les relations sexuelles.',
+    'attraction.o.name': 'Sexualité tertiaire',
+    'attraction.o.description': 'Ne ressent pas d\'attirance sexuelle mais est à l\'aise avec les relations ' +
+                                'sexuelles pour certains buts (faire un enfant, donner du plaisir à leur(s) partenaire(s), etc.)',
+    'attraction.p.name': 'Sexualité secondaire',
+    'attraction.p.description': 'Il est facile de ressentir une attraction romantique, platonique et/ou physique. ' +
+                                'L\'attirance sexuelle se développe au cours de la relation, pas immédiatement.',
+    'attraction.r.name': 'Sexualité primaire',
+    'attraction.r.description': 'L\'attirance sexuelle existe immédiatement et il peut y avoir un passage à l\'acte ou non. ' +
+                                'Cependant d\'autres éléments sont essentiels à la relation comme le fait d\'avoir noué des liens.',
+    'attraction.c.name': 'Sexualité aromantique',
+    'attraction.c.description': 'Le but des relations est principalement de faire du sexe. ' +
+                                'Les sentiments romantiques ne sont pas forcément désirés.',
+
+    'relationship.label': 'Type de relation',
+    'relationship.A.name': 'Strictement monogame',
+    'relationship.A.description': 'N\'a qu\'un⋅e seul⋅e partenaire en même temps ' +
+                                    'dans le but de développer une seule relation devant durer toute la vie.',
+    'relationship.B.name': 'Principalement monogame',
+    'relationship.B.description': 'Désire avant tout des relations monogames ' +
+                                    'mais a la volonté d\'explorer des relations polyamoureuses pour rendre leur partenaire heureux⋅ses ' +
+                                    'ou pour tout autres raisons.',
+    'relationship.C.name': 'Pas de préférence',
+    'relationship.C.description': 'D\'accord pour explorer le polyamour ou la monogamie au cas par cas ' +
+                                    'selon les dynamiques souhaitées par les partenaires.',
+    'relationship.D.name': 'Principalement polyamoureuse',
+    'relationship.D.description': 'Désire avant tout des relations polyamoureuses ' +
+                                    'mais a la volonté d\'explorer une relation monogame pour rendre leur partenaire heureux⋅ses ' +
+                                    'ou pour tout autres raisons.',
+    'relationship.E.name': 'Strictement polyamoureuse',
+    'relationship.E.description': 'Désire des relations différentes et simultanées avec différent⋅e⋅s partenaires ' +
+                                    'dans le but de remplir différents besoins ou d\'exprimer librement son attirance pour plusieurs personnes.',
+
+    'orientation.label': 'Orientation sexuelle',
+    'orientation.0.name': 'Exclusivement hétérosexuel⋅le',
+    'orientation.0.description': 'Exclusivement attiré⋅e par un ou des genre(s) différent(s) du sien.',
+    'orientation.1.name': 'Principalement hétérosexuel⋅le',
+    'orientation.1.description': 'En majorité attiré⋅e par un ou des genre(s) différent(s) du sien.',
+    'orientation.2.name': 'Légèrement hétérosexuel⋅le',
+    'orientation.2.description': 'Préfère les partenaires dont le genre est différent du sien ' +
+                                    'mais est également attiré⋅e par les personnes du même genre.',
+    'orientation.3.name': 'Bisexuel⋅le / Pansexuel⋅le',
+    'orientation.3.description': 'L\'attirance est indépendante du genre des personnes.',
+    'orientation.4.name': 'Légèrement homosexuel⋅le',
+    'orientation.4.description': 'Préfère les partenaires dont le genre est le même que le sien, ' +
+                                    'mais est également attiré⋅e par les personnes dont le genre est différent du sien.',
+    'orientation.5.name': 'Principalement homosexuel⋅le',
+    'orientation.5.description': 'En majorité attiré⋅e par les personnes du même genre.',
+    'orientation.6.name': 'Exclusivement homosexuel⋅le',
+    'orientation.6.description': 'Exclusivement attiré⋅e par les personnes du même genre.',
+
+    'show.own': 'Sélectionnez votre propre morceau',
+
+    'url.empty': 'Sélectionnez une option dans chacune des trois colonnes pour générer un lien.',
+    'url.copy': 'Copier dans le presse-papier',
+
+    'share.text': 'Mon type d\'attirance est {code}.',
+    'share.text.rich': 'Mon type d\'attirance est <strong>{code}</strong>.',
+    'share.facebook': 'Partager sur Facebook',
+    'share.twitter': 'Partager sur Twitter',
+
+    'privacy.header': 'Vie privée',
+    'privacy.noData': 'Toutes les informations que vous avez sélectionnées sont encodées dans l\'URL et ne sont stockées nulle part.',
+    'privacy.tracking': 'J\'utilise Mamoto pour analyser les passages sur le site. Toutes les données des visiteureuses sont anonymisées.',
+    'privacy.contact': 'Si vous avez la moindre question ou demande à propos de vos données personnelles, n\'hésitez pas à me contacter sur',
+
+    'otherProjects.spectrum': 'Spectre de la sexualité',
+};
+
