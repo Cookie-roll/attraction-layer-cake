84721528e028d653c12a4f26a681747936434e0e
Andrea Vos <andrea@avris.it>
2020-06-29 20:30:21 +0000

    [trans] pl - change y/a -> x notation

--- src/translations/pl.js
+++ src/translations/pl.js
@@ -13,10 +13,10 @@ module.exports = {
     'attraction.b.name': 'Aseksualność aromantyczna',
     'attraction.b.description': 'Brak prociągu seksualnego i romantycznego, pociąg jedynie platoniczy lub estetyczny.',
     'attraction.g.name': 'Aseksualność romantyczna',
-    'attraction.g.description': 'Otwarta/y na romans, pewne rodzaje dotyku i głębokie więzi emocjonalne, ' +
-                                'ale nie zainteresowana/y związkami seksualnymi.',
+    'attraction.g.description': 'Otwartx na romans, pewne rodzaje dotyku i głębokie więzi emocjonalne, ' +
+                                'ale nie zainteresowanx związkami seksualnymi.',
     'attraction.o.name': 'Seksualność trzeciorzędna',
-    'attraction.o.description': 'Nie odczuwa pociągu seksualnego, ale otwarta/y na stosunki seksaulne ' +
+    'attraction.o.description': 'Nie odczuwa pociągu seksualnego, ale otwartx na stosunki seksaulne ' +
                                 'dla konkretnych celów (prokreacja, uszczęśliwianie partnerów, itp.)',
     'attraction.p.name': 'Seksualność drugorzędna',
     'attraction.p.description': 'Więź romantyczna, platoniczna i estetyczna są łatwe do nawiązania, ' +
@@ -29,41 +29,41 @@ module.exports = {
                                 'Romans nie jest szczególnie pożądany.',
 
     'relationship.label': 'Typ związku',
-    'relationship.A.name': 'Ściśle monogamiczna/y',
+    'relationship.A.name': 'Ściśle monogamicznx',
     'relationship.A.description': 'Wchodzi w związki z jedną osobą na raz ' +
                                     'w celu zbudowania jednego związku na całe życie.',
-    'relationship.B.name': 'Głównie monogamiczna/y',
+    'relationship.B.name': 'Głównie monogamicznx',
     'relationship.B.description': 'Pragnie przede wszystkim związków monogamicznych, ' +
-                                    'ale jest otwarta/y na poliamorię dla uszczęśliwienia partnerki/a ' +
+                                    'ale jest otwartx na poliamorię dla uszczęśliwienia partnerki/a ' +
                                     'lub z innych powodów.',
     'relationship.C.name': 'Bez preferencji',
-    'relationship.C.description': 'Otwarta/y zarówno na poliamorię, jak i monogamię, indywidualnie dla każdego przypadku ' +
+    'relationship.C.description': 'Otwartx zarówno na poliamorię, jak i monogamię, indywidualnie dla każdego przypadku ' +
                                     'w zależności od pragnień zaangażowanych partnerów.',
-    'relationship.D.name': 'Głównie poliamoryczna/y',
+    'relationship.D.name': 'Głównie poliamorycznx',
     'relationship.D.description': 'Pragnie przede wszystkim związków poliamorycznych, ' +
-                                    'ale jest otwarta/y na monogamię dla uszczęśliwienia partnerki/a ' +
+                                    'ale jest otwartx na monogamię dla uszczęśliwienia partnerki/a ' +
                                     'lub z innych powodów.',
-    'relationship.E.name': 'Ściśle poliamoryczna/y',
+    'relationship.E.name': 'Ściśle poliamorycznx',
     'relationship.E.description': 'Pożąda wielu jednoczesnych związków z różnymi partnerami ' +
                                     'w celu zaspokojenia różnych potrzeb i swobodnego okazywania pociągu wielu osobom.',
 
     'orientation.label': 'Typ orientacji',
-    'orientation.0.name': 'Wyłącznie heteroseksualna/y',
-    'orientation.0.description': 'Zainteresowana/y wyłącznie osobami odmiennej/ych płci.',
-    'orientation.1.name': 'Głównie heteroseksualna/y',
-    'orientation.1.description': 'Zainteresowana/y głównie osobami odmiennej/ych płci.',
-    'orientation.2.name': 'Lekko heteroseksuualna/y',
+    'orientation.0.name': 'Wyłącznie heteroseksualnx',
+    'orientation.0.description': 'Zainteresowanx wyłącznie osobami odmiennej/ych płci.',
+    'orientation.1.name': 'Głównie heteroseksualnx',
+    'orientation.1.description': 'Zainteresowanx głównie osobami odmiennej/ych płci.',
+    'orientation.2.name': 'Lekko heteroseksualnx',
     'orientation.2.description': 'Woli partnerki/ów odmiennej/ych płci, ' +
-                                    'ale również zainteresowana/y osobami tej samej płci.',
-    'orientation.3.name': 'Biseksualna/y / Panseksualny/a',
+                                    'ale również zainteresowanx osobami tej samej płci.',
+    'orientation.3.name': 'Biseksualnx / Panseksualnx',
     'orientation.3.description': 'Pociąg nie jest uzależniony od płci.',
-    'orientation.4.name': 'Lekko homoseksualna/y',
+    'orientation.4.name': 'Lekko homoseksualnx',
     'orientation.4.description': 'Woli partnerki/ów tej samej płci, ' +
-                                    'ale również zainteresowana/y osobami odmiennej/ych płci.',
-    'orientation.5.name': 'Głównie homoseksualna/y',
-    'orientation.5.description': 'Zainteresowana/y głównie osobami tej samej płci.',
-    'orientation.6.name': 'Wyłącznie homoseksualna/y',
-    'orientation.6.description': 'Zainteresowana/y wyłącznie osobami tej samej płci.',
+                                    'ale również zainteresowanx osobami odmiennej/ych płci.',
+    'orientation.5.name': 'Głównie homoseksualnx',
+    'orientation.5.description': 'Zainteresowanx głównie osobami tej samej płci.',
+    'orientation.6.name': 'Wyłącznie homoseksualnx',
+    'orientation.6.description': 'Zainteresowanx wyłącznie osobami tej samej płci.',
 
     'show.own': 'Też wybierz swój kawałek',
 
@@ -76,9 +76,9 @@ module.exports = {
     'share.twitter': 'Udostępnij na Twitterze',
 
     'privacy.header': 'Prywatność',
-    'privacy.noData': 'Wszystkie dane o opcjach, które zaznaczyłaś/eś, są zakodowane w URL-u i nie są nigdzie zapisywane.',
+    'privacy.noData': 'Wszystkie dane o opcjach, które zaznaczyłxś, są zakodowane w URL-u i nie są nigdzie zapisywane.',
     'privacy.tracking': 'Używam Matomo do analizy ruchu na stronie. Wszystkie dane odwiedzających są anonimizowane.',
-    'privacy.contact': 'Jeśli maszs pytania lub wnioski na temat Twoich danych osobowych, możesz skontaktować się ze mną przez ',
+    'privacy.contact': 'Jeśli masz pytania lub wnioski na temat Twoich danych osobowych, możesz skontaktować się ze mną przez ',
     'privacy.home': 'Wróć na stronę główną',
 };
 
